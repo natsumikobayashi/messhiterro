d3e6c55bface78465ad7408dc6ac5222c822c02a
NatsumiKobayashi <115065917+natsumikobayashi@users.noreply.github.com>
2022-11-14 14:03:50 +0000

    12章【投稿機能の作成(5) - 投稿ごとの画面を作ろう】

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,12 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
-  belings_to :user
+  belongs_to :user
+  
+  def get_image #特定の処理を名前で呼び出し何かしらの画像を表示させる
+    unless image.attached? #結果がfalseの場合に処理実行
+      file_path = Rails.root.join('app/assets/images/no_image.jpg') #app/assets/image/no_imagesに格納されるno_image.jpgという画像をデフォルト画像として
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg') #ActiveStorageに格納し、格納した画像を表示させる
+    end
+      image #結果がtrueの場合に処理実行
+  end
 end
