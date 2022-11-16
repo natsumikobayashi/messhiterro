fd7a35129fd55cbdcd10ff483a98538147a5a049
NatsumiKobayashi <115065917+natsumikobayashi@users.noreply.github.com>
2022-11-16 02:20:41 +0000

    ログイン後投稿一覧ページへ遷移するよう記述

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -4,15 +4,14 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new #画像投稿のための空箱用意
   end
   
-  def create　#投稿データの保存
+  def index
+    @post_images = PostImage.all
+     def create　#投稿データの保存
     @post_image = PostImage.new(post_image_params) #投稿するデータをPostImageモデルに紐づくデータとして保存する準備
     @post_image.user_id = current_user.id #@post_imageのuser_idをcurrent_user.id(今ログインしているユーザーのID)に指定することで投稿で0タに、今ログイン中のユーザのIDを持たせることができる
     @post_image.save
     redirect_to post_images_path #投稿一覧画面へリダイレクト
-  end
-  
-  def index
-    @post_images = PostImage.all
+    end
   end
 
   def show
