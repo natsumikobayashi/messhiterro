0b0b8c5f91963981725d4aa9b77b329076098093
NatsumiKobayashi <115065917+natsumikobayashi@users.noreply.github.com>
2022-11-20 11:46:37 +0000

    いいね機能のエラー解決

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,8 +1,8 @@
 class PostCommentsController < ApplicationController
   def create
-    post_image = PostImage.find(params[:post_image_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_image_id = post_image.id
+    post_image = PostImage.find(params[:post_image_id]) #コメントを紐づけるPostImageを見つける
+    comment = current_user.post_comments.new(post_comment_params) #コメントをログインしてるユーザーの投稿したコメントたちに保存する準備
+    comment.post_image_id = post_image.id #コメントとコメントされたpost_image_idをpost_image.idに持たせる
     comment.save
     redirect_to post_image_path(post_image)
   end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,6 +2,7 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   def get_image #特定の処理を名前で呼び出し何かしらの画像を表示させる
     unless image.attached? #結果がfalseの場合に処理実行
@@ -9,5 +10,9 @@ class PostImage < ApplicationRecord
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg') #ActiveStorageに格納し、格納した画像を表示させる
     end
       image #結果がtrueの場合に処理実行
+  end
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
 end
