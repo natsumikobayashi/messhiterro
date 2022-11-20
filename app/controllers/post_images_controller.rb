89aaebdf537f271aff24c182dcb09d563b60db87
NatsumiKobayashi <115065917+natsumikobayashi@users.noreply.github.com>
2022-11-20 14:00:24 +0000

    21章【バリデーション・権限を設定しよう】

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,8 +7,11 @@ class PostImagesController < ApplicationController
   def create #投稿データの保存
     @post_image = PostImage.new(post_image_params) #投稿するデータをPostImageモデルに紐づくデータとして保存する準備
     @post_image.user_id = current_user.id #@post_imageのuser_idをcurrent_user.id(今ログインしているユーザーのID)に指定することで投稿で0タに、今ログイン中のユーザのIDを持たせることができる
-    @post_image.save
+    if @post_image.save
     redirect_to post_images_path #投稿一覧画面へリダイレクト
+    else
+    render :new
+    end
   end
   
   def index
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,9 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
+  validates :shop_name, presence: true
+  validates :image, presence: true
+  
   def get_image #特定の処理を名前で呼び出し何かしらの画像を表示させる
     unless image.attached? #結果がfalseの場合に処理実行
       file_path = Rails.root.join('app/assets/images/no_image.jpg') #app/assets/image/no_imagesに格納されるno_image.jpgという画像をデフォルト画像として
